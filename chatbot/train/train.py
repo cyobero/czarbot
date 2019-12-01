4b6527532b38f6686b863c953bb51e515cbc59ba
Czar Yobero <czar.yobero@Czars-MacBook-Pro.local>
2019-11-30 18:56:35 -0800

    added chatbot/train

---
+++ chatbot/train/train.py
@@ -0,0 +1,72 @@
+import tensorflow as tf
+tf.keras.backend.clear_session()
+from transformer import transformer
+
+# Hyper-parameters
+NUM_LAYERS = 2
+D_MODEL = 256
+NUM_HEADS = 8
+UNITS = 512
+DROPOUT = 0.1
+
+model = transformer(
+    vocab_size = VOCAB_SIZE,
+    num_layers = NUM_LAYERS,
+    units      = UNITS,
+    d_model    = D_MODEL,
+    num_heads  = NUM_HEADS,
+    dropout    = DROPOUT
+)
+
+
+# Loss function
+# Since the target sequences are padded, it is important 
+# to apply a padding mask when calculating the loss.
+def loss_function(y_true, y_pred):
+    y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH-1))
+
+    loss = tf.keras.losses.SparseCategoricalCrossEntropy(
+        from_logits=True, reduction='none')(y_true, y_pred)
+
+    mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+    loss = tf.multiply(loss, mask)
+
+    return tf.reduce_mean(loss)
+
+
+# Custom learning rate
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+
+  def __init__(self, d_model, warmup_steps=4000):
+    super(CustomSchedule, self).__init__()
+
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps**-1.5)
+
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+# Compile Model
+learning_rate = CustomSchedule(D_MODEL)
+
+optimizer = tf.keras.optimizers.Adam(
+    learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+
+def accuracy(y_true, y_pred):
+  # ensure labels have shape (batch_size, MAX_LENGTH - 1)
+  y_true = tf.reshape(y_true, shape=(-1, MAX_LENGTH - 1))
+  return tf.keras.metrics.sparse_categorical_accuracy(y_true, y_pred)
+
+model.compile(optimizer=optimizer, loss=loss_function, metrics=[accuracy])
+
+
+# Fit model
+EPOCHS = 20
+
+model.fit(dataset, epochs=EPOCHS)
